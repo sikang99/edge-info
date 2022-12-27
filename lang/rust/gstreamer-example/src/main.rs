92f09150b8720cad48be206a745a89b00443e3f7
Sung Il Kang <sikang99@gmail.com>
2022-12-27 13:48:21 +0900

    0.0.6.6,stoney

--- lang/rust/gstreamer-example/src/main.rs
+++ lang/rust/gstreamer-example/src/main.rs
@@ -14,21 +14,21 @@ fn main() {
 
     // Wait for the pipeline to complete
     let bus = pipeline.get_bus().unwrap();
-    // while let Some(msg) = bus.timed_pop(gst::CLOCK_TIME_NONE) {
-    //     use gst::MessageView;
-    //     match msg.view() {
-    //         MessageView::Eos(..) => break,
-    //         MessageView::Error(err) => {
-    //             println!(
-    //                 "Error from {}: {}",
-    //                 err.get_src().map(|s| s.get_path_string()).unwrap_or("None".to_owned()),
-    //                 err.get_error()
-    //             );
-    //             break;
-    //         }
-    //         _ => (),
-    //     }
-    // }
+    while let Some(msg) = bus.timed_pop(gst::CLOCK_TIME_NONE) {
+        use gst::MessageView;
+        match msg.view() {
+            MessageView::Eos(..) => break,
+            MessageView::Error(err) => {
+                println!(
+                    "Error from {}: {}",
+                    err.get_src().map(|s| s.get_path_string()).unwrap_or("None".to_owned()),
+                    err.get_error()
+                );
+                break;
+            }
+            _ => (),
+        }
+    }
 
     // Set the pipeline back to the NULL state
     pipeline.set_state(gst::State::Null).unwrap();
